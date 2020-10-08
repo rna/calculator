1013f477cb6b75ed3781a154531272562551a21f
Ramesh <rameshnaidu.allu@gmail.com>
2020-10-08 09:21:02 +0530

    add styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,9 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Button(props) {
-  const { name } = props;
+  const { name, wide, color } = props;
+  const styleClasses = [wide ? 'wide' : '', color].join(' ');
+
   return (
-    <button type="button">
+    <button type="button" className={styleClasses}>
       {name}
     </button>
   );
@@ -12,10 +14,14 @@ function Button(props) {
 
 Button.propTypes = {
   name: PropTypes.string,
+  wide: PropTypes.bool,
+  color: PropTypes.string,
 };
 
 Button.defaultProps = {
   name: '',
+  wide: '',
+  color: 'dark',
 };
 
 export default Button;
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -3,17 +3,17 @@ import Button from './Button';
 
 function ButtonPanel() {
   const group1 = ['AC', '+/-', '%', '÷'];
-  const group2 = ['7', '8', '9', 'X'];
+  const group2 = ['7', '8', '9', 'x'];
   const group3 = ['4', '5', '6', '-'];
   const group4 = ['1', '2', '3', '+'];
   const group5 = ['0', '.', '='];
   return (
-    <div>
-      <div id="row-1">{group1.map(element => <Button key={element} name={element} />)}</div>
-      <div id="row-2">{group2.map(element => <Button key={element} name={element} />)}</div>
-      <div id="row-3">{group3.map(element => <Button key={element} name={element} />)}</div>
-      <div id="row-4">{group4.map(element => <Button key={element} name={element} />)}</div>
-      <div id="row-5">{group5.map(element => <Button key={element} name={element} />)}</div>
+    <div className="button-panel">
+      <div id="row-1">{group1.map(element => <Button key={element} name={element} color={element !== '÷' ? 'light' : 'dark'} />)}</div>
+      <div id="row-2">{group2.map(element => <Button key={element} name={element} color={element !== 'x' ? 'light' : 'dark'} />)}</div>
+      <div id="row-3">{group3.map(element => <Button key={element} name={element} color={element !== '-' ? 'light' : 'dark'} />)}</div>
+      <div id="row-4">{group4.map(element => <Button key={element} name={element} color={element !== '+' ? 'light' : 'dark'} />)}</div>
+      <div id="row-5">{group5.map(element => <Button key={element} name={element} color={element !== '=' ? 'light' : 'dark'} wide={element === '0'} />)}</div>
     </div>
   );
 }
