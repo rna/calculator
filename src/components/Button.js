68a9a3a940137feba3ea025945fe9bd5041a4589
Ramesh <rameshnaidu.allu@gmail.com>
2020-10-08 09:21:02 +0530

    add styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ name }) => (
-  <button type="button">
-    {name}
-  </button>
-);
+const Button = ({ name, wide, color }) => {
+  const styleClasses = [wide ? 'wide' : '', color].join(' ');
+  return (
+    <button type="button" className={styleClasses}>
+      {name}
+    </button>
+  );
+};
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
+  wide: PropTypes.bool,
+  color: PropTypes.string,
+};
+
+Button.defaultProps = {
+  wide: '',
+  color: 'dark',
 };
 
 export default Button;
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Button from './Button';
 
-function ButtonPanel() {
-  const groups = [['AC', '+/-', '%', '÷'], ['7', '8', '9', 'X'], ['4', '5', '6', '-'], ['1', '2', '3', '+'], ['0', '.', '=']];
+const ButtonPanel = () => {
+  const groups = [['AC', '+/-', '%', '÷'], ['7', '8', '9', 'x'], ['4', '5', '6', '-'], ['1', '2', '3', '+'], ['0', '.', '=']];
+  const operators = ['÷', 'x', '-', '+', '='];
   return (
     <div>
-      {groups.map(group => <div key="group">{group.map(element => <Button key={element} name={element} />)}</div>)}
+      {groups.map(group => <div key="group">{group.map(element => <Button key={element} name={element} color={operators.indexOf(element) === -1 ? 'light' : 'dark'} wide={element === '0'} />)}</div>)}
     </div>
   );
-}
+};
 
 export default ButtonPanel;
