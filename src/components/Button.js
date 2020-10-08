208746a3ecd51456e714328ca7663f9369b9e83b
Ramesh <rameshnaidu.allu@gmail.com>
2020-10-08 09:36:35 +0530

    change button component to ES6

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Button(props) {
-  const { name } = props;
-  return (
-    <button type="button">
-      {name}
-    </button>
-  );
-}
+const Button = ({ name }) => (
+  <button type="button">
+    {name}
+  </button>
+);
 
 Button.propTypes = {
-  name: PropTypes.string,
-};
-
-Button.defaultProps = {
-  name: '',
+  name: PropTypes.string.isRequired,
 };
 
 export default Button;
