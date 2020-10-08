cd7e9ee5551cdc4d406c96f6b5ec050fad5ff8a9
Ramesh <rameshnaidu.allu@gmail.com>
2020-10-08 09:48:38 +0530

    refactor buttonPanel component

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -2,18 +2,10 @@ import React from 'react';
 import Button from './Button';
 
 function ButtonPanel() {
-  const group1 = ['AC', '+/-', '%', '÷'];
-  const group2 = ['7', '8', '9', 'X'];
-  const group3 = ['4', '5', '6', '-'];
-  const group4 = ['1', '2', '3', '+'];
-  const group5 = ['0', '.', '='];
+  const groups = [['AC', '+/-', '%', '÷'], ['7', '8', '9', 'X'], ['4', '5', '6', '-'], ['1', '2', '3', '+'], ['0', '.', '=']];
   return (
     <div>
-      <div id="row-1">{group1.map(element => <Button key={element} name={element} />)}</div>
-      <div id="row-2">{group2.map(element => <Button key={element} name={element} />)}</div>
-      <div id="row-3">{group3.map(element => <Button key={element} name={element} />)}</div>
-      <div id="row-4">{group4.map(element => <Button key={element} name={element} />)}</div>
-      <div id="row-5">{group5.map(element => <Button key={element} name={element} />)}</div>
+      {groups.map(group => <div key="group">{group.map(element => <Button key={element} name={element} />)}</div>)}
     </div>
   );
 }
